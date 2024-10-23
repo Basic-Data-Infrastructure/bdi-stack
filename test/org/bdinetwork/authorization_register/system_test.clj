9c7ee25fdefd06f4b6ae7511564a34c88cbb4d7e
Joost Diepenmaat <joost@zeekat.nl>
2024-10-23 16:33:02 +0200

    Fixup licensing info

--- test/org/bdinetwork/authorization_register/system_test.clj
+++ test/org/bdinetwork/authorization_register/system_test.clj
@@ -1,3 +1,10 @@
+;;; SPDX-FileCopyrightText: 2024 Jomco B.V.
+;;; SPDX-FileCopyrightText: 2024 Topsector Logistiek
+;;; SPDX-FileContributor: Joost Diepenmaat <joost@jomco.nl>
+;;; SPDX-FileContributor: Remco van 't Veer <remco@jomco.nl>
+;;;
+;;; SPDX-License-Identifier: AGPL-3.0-or-later
+
 (ns org.bdinetwork.authorization-register.system-test
   (:require [org.bdinetwork.authorization-register.system :as system]
             [org.bdinetwork.association-register.system :as association]
