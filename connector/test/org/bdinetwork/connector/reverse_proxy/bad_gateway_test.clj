c22de1a52b8219be947d6e3e0d8379254a49a073
Joost Diepenmaat <joost@zeekat.nl>
2025-05-26 15:15:12 +0200

    Ensure exceptions during initialization do not leak resources
    When starting/opening multiple resources, you must take care that the
    resources are closed if exceptions are thrown.

--- connector/test/org/bdinetwork/connector/reverse_proxy/bad_gateway_test.clj
+++ connector/test/org/bdinetwork/connector/reverse_proxy/bad_gateway_test.clj
@@ -6,12 +6,13 @@
   (:require [aleph.http :as http]
             [clojure.test :refer [deftest is use-fixtures]]
             [nl.jomco.http-status-codes :as http-status]
-            [org.bdinetwork.connector.reverse-proxy-test-helper :refer [proxy-url start-proxy stop-proxy]]))
+            [nl.jomco.resources :refer [with-resources]]
+            [org.bdinetwork.connector.reverse-proxy-test-helper :refer [proxy-url start-proxy]]))
 
 (use-fixtures :once
   (fn [f]
-    (let [proxy (start-proxy (fn [& args] (throw (ex-info "boom!" args))))]
-      (try (f) (finally (stop-proxy proxy))))))
+    (with-resources [_proxy (start-proxy (fn [& args] (throw (ex-info "boom!" args))))]
+      (f))))
 
 (deftest bad-gateway
   (let [{:keys [status]} @(http/get proxy-url {:throw-exceptions? false})]
